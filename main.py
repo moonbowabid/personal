6bde242ca6cc22cfca2a0275e9b2f262aab51a34
Abid Malik <malikabid2006@gmail.com>
2024-02-27 17:52:12 +0530

    updated test template

--- main.py
+++ main.py
@@ -4,11 +4,35 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
-    
+import chromedriver_autoinstaller
+from pyvirtualdisplay import Display
+display = Display(visible=0, size=(800, 800))  
+display.start()
+
+chromedriver_autoinstaller.install()
+chrome_options = webdriver.ChromeOptions()    
+# Add your options as needed    
+options = [
+  # Define window size here
+   "--window-size=1200,1200",
+    "--ignore-certificate-errors"
+ 
+    #"--headless",
+    #"--disable-gpu",
+    #"--window-size=1920,1200",
+    #"--ignore-certificate-errors",
+    #"--disable-extensions",
+    #"--no-sandbox",
+    #"--disable-dev-shm-usage",
+    #'--remote-debugging-port=9222'
+]
+
+for option in options:
+    chrome_options.add_argument(option)
 class TestPersonalBrandWebsite(unittest.TestCase):
     def setUp(self):
         # Set up the WebDriver (in this example, using Chrome)
-        self.driver = webdriver.Chrome()
+        self.driver = webdriver.Chrome(options = chrome_options)
         self.base_url = "https://moonbowabid.github.io/personal/"
         self.driver.get(self.base_url)
 
